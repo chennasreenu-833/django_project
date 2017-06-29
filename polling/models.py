33017705dd159d989878b4c844b022d166f50d1f
chennasreenu_833 <sreenu833.chenna@gmail.com>
2017-06-29 23:38:34 +0530

    modified db

--- polling/models.py
+++ polling/models.py
@@ -15,5 +15,16 @@ class Results(models.Model):
     status=models.CharField(max_length=200)
     state=models.CharField(max_length=200)
     state_code=models.CharField(max_length=100)
+class PollTable(models.Model):
+    constituency = models.CharField(max_length=200)
+    constituency_code = models.IntegerField()
+    leading_candidate = models.CharField(max_length=200)
+    leading_party = models.CharField(max_length=200)
+    trailing_candidate = models.CharField(max_length=200)
+    trailing_party = models.CharField(max_length=200)
+    margin = models.IntegerField()
+    status = models.CharField(max_length=200)
+    state = models.CharField(max_length=200)
+    state_code = models.CharField(max_length=100)
 
 # Create your models here.
--- polling/views.py
+++ polling/views.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from .models import Results
+from .models import PollTable
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def get_value(request):
     key=request.GET.get('id','')
-    record=Results.objects.get(id=key)
+    record=PollTable.objects.get(id=key)
     list=[]
     list.append(record.constituency)
     list.append(record.constituency_code)
