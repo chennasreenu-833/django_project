35a4b21a58976f6ca99ca9af7a44dc409bbf81ca
chennasreenu_833 <sreenu833.chenna@gmail.com>
2017-06-29 23:57:39 +0530

    json formatted

--- polling/views.py
+++ polling/views.py
@@ -2,19 +2,23 @@
 from __future__ import unicode_literals
 from .models import PollTable
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 
 def get_value(request):
     key=request.GET.get('id','')
     record=PollTable.objects.get(id=key)
-    list=[]
-    list.append(record.constituency)
-    list.append(record.constituency_code)
-    list.append(record.leading_candidate)
-    list.append(record.leading_party)
-    list.append(record.trailing_candidate)
-    list.append(record.trailing_party)
+    const=record.constituency
+    const_code=record.constituency_code
+    lead_cand=record.leading_candidate
+    lead_party=record.leading_party
+    trial_cand=record.trailing_candidate
+    trail_part=record.trailing_party
+    marg=record.margin
+    status=record.status
+    state=record.state
+    state_c=record.state_code
+    dict={"constitueny":const,"constituency-code":const_code,"Leading-Candidate":lead_cand,"Leading-Party":lead_party,"Trailing-Candidate":trial_cand,"Trailing_Party":trail_part,"Margin":marg,"Status":status,"State":state,"State-Code":state_c}
 
-    return HttpResponse(list)
+    return JsonResponse(dict)
 
 # Create your views here.
